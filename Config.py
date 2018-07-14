b396ea4074cd5c9d3c5a3593434b447d8167ac4f
Komal Dhuri <komal.a.dhuri@gmail.com>
2018-07-14 16:28:14 -0700

    Configurations for hyperparameters

---
+++ Config.py
@@ -0,0 +1,24 @@
+UNKNOWN = "UNK"
+ROOT = "ROOT"
+NULL = "NULL"
+NONEXIST = -1
+
+max_iter = 3001   ######## My Best #############
+#max_iter = 1001  ######## Defalut #############
+
+batch_size = 10000
+hidden_size = 200
+hidden_size_2 = 200
+
+embedding_size = 50
+learning_rate = 0.1
+
+display_step = 100
+validation_step = 200
+n_Tokens = 48
+############ For Parallel Layers ###############
+n_Tokens_words = 18
+n_Tokens_pos = 18
+n_Tokens_labels = 12
+###############################################
+lam = 1e-8
